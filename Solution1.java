c1750120fde8d9f455de6ad1db439b7bf367ba09
Rowan Mueller <rowanmuelle@gmail.com>
2025-08-02 16:33:35 -0400

    added leetcode75 folder, solution 1 added

---
+++ Solution1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution1 {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = Arrays.stream(candies).max().getAsInt();
+
+        List<Boolean> candiesArr=new ArrayList<>();
+
+        for(int i=0 ; i<candies.length; i++){
+            if(candies[i]<(max-extraCandies)){
+                candiesArr.add(false);
+            }
+            else{
+                candiesArr.add(true);
+            }
+        }
+
+        return candiesArr;
+    }
+}
