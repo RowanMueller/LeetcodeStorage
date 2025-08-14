50f947efb66db4cfb67952843a22ba637836717d
Rowan Mueller <rowanmuelle@gmail.com>
2025-08-13 22:34:28 -0400

    semantic changes

--- Problems/Linked Lists/ArrayLists.java
+++ Problems/Linked Lists/ArrayLists.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class Solution {
+class KidsWithCandies {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int max = Arrays.stream(candies).max().getAsInt();
 
-        List<Boolean> candiesArr=new ArrayList();
+        List<Boolean> candiesArr=new ArrayList<>();
 
         for(int i=0 ; i<candies.length; i++){
             if(candies[i]<(max-extraCandies)){
